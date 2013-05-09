c27f304ee9ddd3cc5cc3a8acf83dd3e99ccb8398
Matthew Evans <add1ct3dd@gmail.com>
2013-05-09 19:32:56 +0100

    Brackets refusing to save, thank you for that, finaly renaming.

--- faq.php
+++ faq.php
@@ -12,27 +12,27 @@
     <thead>
     	<tr>
 			<th>Whats this ?</th></tr><tr>
-			<td>Anubis is a web frontend for cgminer (<a href="https://bitcointalk.org/index.php?topic=28402.0">https://bitcointalk.org/index.php?topic=28402.0</a>) a bitcoin miner for windows/linux.
-			Anubis "watches" your hosts by connecting to the API Port of cgminer.</td>
+			<td>AnubisApp is a web frontend for cgminer (<a href="https://bitcointalk.org/index.php?topic=28402.0">https://bitcointalk.org/index.php?topic=28402.0</a>) a bitcoin miner for windows/linux.
+			AnubisApp "watches" your hosts by connecting to the API Port of cgminer.</td>
         </tr>
     	<tr>
 			<th>How Do I enable it ?</th></tr><tr>
 			<td>The Connection is very simple, just add "--api-listen" (and "--api-network") to the cgminer command line and cgminer's api is enabled.
-			After installing Anubis simply start by <a href="addhost.php">adding some hosts.</a></td>
+			After installing AnubisApp simply start by <a href="addhost.php">adding some hosts.</a></td>
         </tr>
     	<tr>
 			<th>Something is wrong/does not work as expected.</th></tr><tr>
-			<td>Since we are in a very early development stage of Anubis there will surely be bugs. I'll start a git repo in short for Anubis and hope
+			<td>Since we are in a very early development stage of AnubisApp there will surely be bugs. I'll start a git repo in short for AnubisApp and hope
 			this wil speed bugfixes up a little. For the moment, keep checking <a href="https://bitcointalk.org/index.php?board=42.0">https://bitcointalk.org/index.php?board=42.0</a>
-			for new messages and/or bugfixes concerning Anubis</td>
+			for new messages and/or bugfixes concerning AnubisApp</td>
         </tr>
     	<tr>
 			<th>Installation ?</th></tr><tr>
 			<td>All you need is a php/mysql enabled host. This host has to be able to reach your miners by network i.e. you should be
-			able to ping your miners from the php/mysql enabled host. Simple copy all the Anubis files into a directory of your choice
+			able to ping your miners from the php/mysql enabled host. Simple copy all the AnubisApp files into a directory of your choice
 			into your webserver root and call it there like: http://my.host.com/anubis i.e.<BR>
 			<BR>
-			Anubis will need a mysql user/password/database connection. Edit "config.inc.php" and change it to your needs. </td>
+			AnubisApp will need a mysql user/password/database connection. Edit "config.inc.php" and change it to your needs. </td>
         </tr>
         
     </thead>
