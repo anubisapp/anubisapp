78e50dbdbbeb8d0bd542eacce56d6fa829a61e6f
Tristan van Bokkem <tristan@i3d.nl>
2013-05-10 19:33:25 +0200

    deprecate read-only

--- read-only/stylesheets.inc.php
+++
@@ -1,4 +0,0 @@
-<link rel="icon" href="../favicon.ico" />
-<link rel="stylesheet" type="text/css" href="../css/templatemo_style.css" />
-<link rel="stylesheet" type="text/css" href="../css/ddsmoothmenu.css" />
-<link rel="stylesheet" type="text/css" href="../css/acuity.css" />
